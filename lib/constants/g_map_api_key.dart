e212853048f3494943f7d572cfe527660668921e
EsinShadrach <esinshadrachrafe@gmail.com>
2024-02-17 11:34:47 +0100

    Added Icons

---
+++ lib/constants/g_map_api_key.dart
@@ -0,0 +1,3 @@
+mixin Constants {
+  static const googleMapKey = "AIzaSyDUKXoTXBPiuoC1q_GCnYWoHkuGKKuo_oU";
+}
---
+++ lib/widget/card.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LocationCard extends StatelessWidget {
+  const LocationCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
